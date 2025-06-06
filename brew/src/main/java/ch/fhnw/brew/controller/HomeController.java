15886c3422679a4cafa404d59feeff2e31e2aa1d
Claudia Graf <146426000+ClaGra@users.noreply.github.com>
2025-06-06 15:51:13 +0000

    Commit 06.06.2025

--- brew/src/main/java/ch/fhnw/brew/controller/HomeController.java
+++ brew/src/main/java/ch/fhnw/brew/controller/HomeController.java
@@ -1,7 +1,9 @@
 package ch.fhnw.brew.controller;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
 
 @RestController
 public class HomeController {
@@ -9,5 +11,12 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "Happy Brewing!";
+    }
+
+    @GetMapping(value="/user")
+    public String getUserRole(Authentication auth) {
+        UserDetails userDetails = (UserDetails) auth.getPrincipal();
+        String role = userDetails.getAuthorities().toArray()[1].toString();
+        return role;
     }
 }
